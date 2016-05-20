a8cf81cda425d2e064f42153575e1fc3deb88fc4
Anthony <anthony.bastin@orange.fr>
2016-05-20 16:33:55 +0200

    zzz

---
+++ WebContent/index.jsp
@@ -0,0 +1,28 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+    pageEncoding="ISO-8859-1"%>
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<%@taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c"%>
+<html>
+	<head>
+		<title>Farming Site</title>
+		<meta http-equiv="Content-Language" content="French" />
+		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+	</head>
+<body>
+	<div id="header">
+		<h1>Site de programmation de farm</h1>
+	</div>
+
+	<div id="content">
+		<h2>Connexion</h2>
+		<div>
+		    <span class="label label-danger">${error}</span>
+			<FORM METHOD=POST ACTION="DALConnexion">
+			<p>Login : <INPUT TYPE=TEXT NAME="login"></p>
+			<p>Password  : <INPUT TYPE="password" NAME="pass"></p>
+			<INPUT TYPE=SUBMIT VALUE="Connexion">
+			</FORM>
+		</div>
+	</div>
+</body>
+</html>
--- src/Controller/DALConnexion.java
+++ src/Controller/DALConnexion.java
@@ -11,7 +11,6 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
-import Exception.*;
 import Exception.Exception;
 import Métier.Connexion;
 import Métier.Utilisateur;
@@ -20,27 +19,30 @@ public class DALConnexion extends HttpServlet {
 	
 	private static final long serialVersionUID = -2135238259758756344L;
 
+	// Méthode doGet qui permet de récupérer les informations de la page précédente (non utilisée du coup)
 	@Override
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		request.getRequestDispatcher("/index.html")
 			.forward(request, response);
 	}
 
+	// Méthode doPost qui est utilisée lors du clic sur le bouton se connecter 
 	@Override
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		
 		Utilisateur monUtil;
-		boolean ok = false;
 		String pseudo = request.getParameter("login");
 		String mdp = request.getParameter("pass");
 		if (pseudo.equals("")) {
-			ok = false;
-			throw new Exception("Le pseudo ne peut pas être vide");
+			Exception exc = new Exception("Le pseudo ne doit pas être vide.");
+			request.setAttribute("error", exc.getMessage());
+			   response.sendRedirect("index.jsp");
 		}
 		else {
 			if (mdp.equals("")) {
-				ok = false;
-				throw new Exception("Le mot de passe ne peut pas être vide");
+				Exception exc = new Exception("Le mot de passe ne doit pas être vide.");
+				request.setAttribute("error", exc.getMessage());
+				   response.sendRedirect("index.jsp");
 			}
 			else {
 				try {
@@ -51,7 +53,7 @@ public class DALConnexion extends HttpServlet {
 				}
 				catch (Exception Exception) {
 					 request.setAttribute("error", Exception.getMessage());
-					   response.sendRedirect("index.html");
+					   response.sendRedirect("index.jsp");
 				}
 			}
 		}
